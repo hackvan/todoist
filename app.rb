9b83d970c24b7597bfdc3aa34cd73f4416d6cc6d
Diego Camacho <hackvan@gmail.com>
2017-07-01 22:44:59 -0500

    Adding functionality to done tasks and delete.

--- app.rb
+++ app.rb
@@ -1,8 +1,40 @@
 require 'sinatra'
 require 'make_todo'
 
+set :sessions, true
+
 get '/' do
+  redirect '/tasks'
+end
+
+get '/tasks/?' do
   @title = "Home"
   @task  = Tarea.all
+  @task.sort!{ |a, b| b["created_at"] <=> a["created_at"] }
+  @flash = session[:flash]
+  session[:flash] = nil
   erb :index
+end
+
+post '/task/new' do
+  Tarea.create(params[:task])
+  session[:flash] = {
+    message: "The task has been created",
+    type: "success"
+  }
+  redirect '/tasks'
+end
+
+get '/task/:id/done' do
+  Tarea.update(params[:id].to_i)
+  redirect '/tasks'
+end
+
+get '/task/:id/delete' do
+  Tarea.destroy(params[:id].to_i)
+  session[:flash] = {
+    message: "The task has been deleted",
+    type: "info"
+  }
+  redirect '/tasks'
 end
